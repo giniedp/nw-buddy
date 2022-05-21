340fef88ffd83da2e2930963b6880e2dad87aebc
Ginie <github@ginie.eu>
2022-05-21 15:46:31 +0200

    fixed tier label

--- apps/web/app/core/nw/utils/tier-to-roman.ts
+++ apps/web/app/core/nw/utils/tier-to-roman.ts
@@ -1,11 +1,11 @@
 const ROMAN = {
   1: 'Ⅰ',
   2: 'Ⅱ',
-  3: 'Ⅱ',
-  4: 'Ⅲ',
+  3: 'Ⅲ',
+  4: 'Ⅳ',
   5: 'Ⅴ',
 }
 
 export function getItemTierAsRoman(tier: number) {
-  return ROMAN[tier] || String(tier ?? '')
+  return ROMAN[tier] || ''
 }
