4341f1f272fda532457641fcf1b90607a11518c9
Ginie <github@ginie.eu>
2022-06-02 14:16:15 +0200

    fixed armorsets detail view

--- apps/web/app/pages/armorsets/armorsets-table.component.ts
+++ apps/web/app/pages/armorsets/armorsets-table.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy, TrackByFunction } from '@angular/core'
+import { Component, OnInit, ChangeDetectionStrategy, TrackByFunction, ViewChild } from '@angular/core'
+import { defer, map } from 'rxjs'
+import { shareReplayRefCount } from '~/core/utils'
+import { DataTableComponent } from '~/ui/data-table'
 import { QuicksearchService } from '~/ui/quicksearch'
+import { Armorset } from './types'
 
 @Component({
   selector: 'nwb-armorsets-table',
@@ -9,7 +13,15 @@ import { QuicksearchService } from '~/ui/quicksearch'
 })
 export class ArmorsetsTableComponent implements OnInit {
   public trackByIndex: TrackByFunction<any> = (i) => i
-  constructor(public search: QuicksearchService) {}
 
-  ngOnInit(): void {}
+  @ViewChild(DataTableComponent, { static: true })
+  public table: DataTableComponent<Armorset>
+
+  public selectedItems = defer(() => this.table.selectedItem)
+    .pipe(map((it) => it?.items))
+    .pipe(shareReplayRefCount(1))
+
+  public constructor(public search: QuicksearchService) {}
+
+  public ngOnInit(): void {}
 }
