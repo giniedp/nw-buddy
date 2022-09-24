fa44eb1e5fb5d81431ce911207bfa88873839eee
Ginie <github@ginie.eu>
2022-09-24 12:03:58 +0200

    prepared for capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'eu.ginie.nwbuddy',
+  appName: 'New World Buddy',
+  webDir: 'dist/web',
+  bundledWebRuntime: false,
+}
+
+export default config
