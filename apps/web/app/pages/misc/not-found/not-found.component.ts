59c127ce2b8a14fd2c31c19c334f58b74082fa65
Alexander Gräfenstein <github@ginie.eu>
2022-11-07 23:32:23 +0100

    updated 404 page

--- apps/web/app/pages/misc/not-found/not-found.component.ts
+++ apps/web/app/pages/misc/not-found/not-found.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'nwb-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
+  host: {
+    class: 'layout-col items-center justify-center'
+  }
 })
 export class NotFoundComponent implements OnInit {
 
