0c0ee31249e87bd2d133889aafcff85f80ab7bb7
Ginie <github@ginie.eu>
2022-12-06 08:36:18 +0100

    updated ptr data

--- apps/web/nw-data/datatables.ts
+++ apps/web/nw-data/datatables.ts
@@ -150,6 +150,9 @@ export abstract class NwDataLoader {
   public itemdefinitionsMasterQuest() {
     return this.load<ItemDefinitionMaster[]>('javelindata_itemdefinitions_master_quest.json')
   }
+  public itemdefinitionsMasterSkins() {
+    return this.load<ItemDefinitionMaster[]>('javelindata_itemdefinitions_master_skins.json')
+  }
   public itemdefinitionsMasterStore() {
     return this.load<ItemDefinitionMaster[]>('javelindata_itemdefinitions_master_store.json')
   }
