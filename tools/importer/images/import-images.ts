94c6486d73e9e22745f3648f714f762847e1a9ff
Ginie <github@ginie.eu>
2023-03-26 16:37:49 +0200

    fixwd mwegw

--- tools/importer/images/import-images.ts
+++ tools/importer/images/import-images.ts
@@ -1,14 +1,10 @@
 import * as path from 'path'
-<<<<<<< HEAD
 import {
   assmebleWorkerTasks,
   replaceExtname,
   withProgressBar,
   withProgressPool as withProgressWorker,
 } from '../../utils'
-=======
-import { assmebleWorkerTasks, replaceExtname, withProgressBar, withProgressPool as withProgressWorker } from '../../utils'
->>>>>>> ptr
 
 import { walkJsonStrings } from '../../utils/walk-json-object'
 import { WORKER_TASKS } from './worker.tasks'
@@ -30,11 +26,7 @@ export async function importImages({
   tables: Object[]
   ignoreKeys: string[]
   rewrite?: Record<string, RewriteImageEntryFn>
-<<<<<<< HEAD
   rewritePath?: (value: string) => string
-=======
-  rewritePath?: (value: string) => string,
->>>>>>> ptr
   threads: number
 }) {
   const images = new Map<string, string>()
