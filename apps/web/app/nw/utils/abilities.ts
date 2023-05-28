540aaf993a5d809a37a7b34f664eaf9a79967ea2
Ginie <github@ginie.eu>
2023-05-28 09:59:37 +0200

    chore: ability trigger matcher

--- apps/web/app/nw/utils/abilities.ts
+++ apps/web/app/nw/utils/abilities.ts
@@ -1,4 +1,5 @@
-import { Ability } from "@nw-data/types";
+import { AttackType, Statuseffect } from '@nw-data/types'
+import { Ability, Affixstats, Damagetable, Damagetypes } from '@nw-data/types'
 
 const WEAPON_TAG_NAME = {
   Sword: 'ui_straightsword',
@@ -6,8 +7,8 @@ const WEAPON_TAG_NAME = {
   Axe: 'ui_hatchet',
   Spear: 'ui_spear',
   GreatAxe: 'ui_greataxe',
-  Warhammer:'ui_warhammer',
-  Greatsword:'ui_greatsword',
+  Warhammer: 'ui_warhammer',
+  Greatsword: 'ui_greatsword',
   Rifle: 'ui_musket',
   Bow: 'ui_bow',
   Blunderbuss: 'ui_blunderbuss',
@@ -18,7 +19,9 @@ const WEAPON_TAG_NAME = {
 }
 
 export function getAbilityCategoryTag(ability: Ability) {
-  return ability?.UICategory?.match(/(heal|melee|debuff|buff|range|magic|passive)/i)?.[0]?.toLowerCase() as any || 'none'
+  return (
+    (ability?.UICategory?.match(/(heal|melee|debuff|buff|range|magic|passive)/i)?.[0]?.toLowerCase() as any) || 'none'
+  )
 }
 
 export function getWeaponTagLabel(value: string) {
@@ -32,4 +35,82 @@ export function stripAbilityProperties(item: Ability): Partial<Ability> {
       it[key] = value
       return it
     }, {})
+}
+
+export function doesAbilityTriggerDamage(
+  ability: Ability,
+  damageRow: Damagetable,
+  damageTypes: Map<string, Damagetypes>,
+  gemDamageType: Affixstats
+) {
+  if (ability.DamageIsRanged && !damageRow.IsRanged) {
+    return false
+  }
+
+  if (ability.DamageIsMelee && damageRow.IsRanged) {
+    return false
+  }
+
+  if (ability.RequireReaction && damageRow.NoReaction) {
+    return false
+  }
+
+  if (ability.DamageTableRow?.length > 0 && !ability.DamageTableRow.includes(damageRow.DamageID)) {
+    return false
+  }
+
+  if (ability.RemoteDamageTableRow?.length > 0 && !ability.RemoteDamageTableRow.includes(damageRow.DamageID)) {
+    return false
+  }
+
+  if (ability.AttackType?.length > 0 && !ability.AttackType.includes(damageRow.AttackType as AttackType)) {
+    return false
+  }
+
+  const rowDamageType = gemDamageType.DamageType || damageRow.DamageType
+
+  if (ability.DamageTypes.length > 0 && !ability.DamageTypes.includes(rowDamageType)) {
+    return false
+  }
+
+  if (ability.DamageCategory && damageTypes.get(rowDamageType)?.Category !== ability.DamageCategory) {
+    return false
+  }
+
+  return true
+}
+
+export function doesAbilityTriggerStatusEffect(
+  ability: Ability,
+  statusEffect: Statuseffect,
+  damageTypes: Map<string, Damagetypes>,
+  target?: boolean
+) {
+  if (ability.RequireReaction) {
+    return false
+  }
+  if (!target) {
+    if (ability.StatusEffect !== statusEffect.StatusID) {
+      return false
+    }
+    if (!ability.StatusEffectCategories.some((cat: string) => statusEffect.EffectCategories.includes(cat))) {
+      return false
+    }
+  } else {
+    if (ability.TargetStatusEffect !== statusEffect.StatusID) {
+      return false
+    }
+    if (!ability.TargetStatusEffectCategory.some((cat: string) => statusEffect.EffectCategories.includes(cat))) {
+      return false
+    }
+  }
+
+  if (ability.DamageTypes && !ability.DamageTypes.includes(statusEffect.DamageType)) {
+    return false
+  }
+  if (ability.DamageCategory && ability.DamageCategory !== damageTypes.get(statusEffect.DamageType)?.Category) {
+    return false
+  }
+
+  return true
 }
