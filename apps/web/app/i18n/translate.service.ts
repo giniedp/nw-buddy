0c02d1a5cf78d38c69c9b1d9745b95eebb1c1d4f
Ginie <github@ginie.eu>
2023-10-19 14:30:18 +0200

    return untransformed translation keys if translation not found

--- apps/web/app/i18n/translate.service.ts
+++ apps/web/app/i18n/translate.service.ts
@@ -27,31 +27,68 @@ export class TranslateService {
     })
       .pipe(
         switchMap(({ key }) => {
-          key = normalizeLookupKey(key)
-          if (Array.isArray(key)) {
-            return combineLatest(key.map((it) => (it ? this.service.get(it) : of(it)))).pipe(map((it) => it.join(' ')))
+          if (!key || !key.length) {
+            return of(null)
           }
-          return key ? this.service.get(key) : of(key)
+          if (Array.isArray(key)) {
+            return combineLatest(key.map((it) => this.getWatched(it))).pipe(map((it) => it.join(' ')))
+          }
+          return this.getWatched(key)
         })
       )
       .pipe(distinctUntilChanged())
   }
 
-  public get(key: string | string[]) {
-    key = normalizeLookupKey(key)
-    if (Array.isArray(key)) {
-      return key
-        .map((it) => {
-          return it ? this.service.instant(it) : it
-        })
-        .join(' ')
+  public get(key: string | string[]): string {
+    if (!key || !key.length) {
+      return null
     }
-    return key ? this.service.instant(key) : key
+    if (Array.isArray(key)) {
+      return key.map((it) => this.getSync(it)).join(' ')
+    }
+    return this.getSync(key)
   }
 
-  public async getAsync(key: string) {
-    key = normalizeLookupKey(key)
-    return firstValueFrom(this.service.get(key))
+  private getSync(key: string): string {
+    if (!key) {
+      return key
+    }
+    const lookupKey = normalizeLookupKey(key)
+    const result = this.service.instant(lookupKey)
+    if (result === lookupKey) {
+      // translation key is missing, return untransformed key
+      return key
+    }
+    return result
+  }
+
+  public async getAsync(key: string): Promise<string> {
+    if (!key) {
+      return key
+    }
+    const lookupKey = normalizeLookupKey(key)
+    const result = await firstValueFrom(this.service.get(lookupKey))
+    if (result === lookupKey) {
+      // translation key is missing, return untransformed key
+      return key
+    }
+    return result
+  }
+
+  private getWatched(key: string): Observable<string> {
+    if (!key) {
+      return of(key)
+    }
+    const lookupKey = normalizeLookupKey(key)
+    return this.service.get(lookupKey).pipe(
+      map((result) => {
+        if (result === lookupKey) {
+          // translation key is missing, return untransformed key
+          return key
+        }
+        return result
+      })
+    )
   }
 
   public use(locale: string) {
