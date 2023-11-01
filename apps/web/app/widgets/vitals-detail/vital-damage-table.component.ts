5ee95640af444e1910c89a0ab659e32c5f91d539
Ginie <github@ginie.eu>
2023-11-01 18:38:09 +0100

    feature: show split damage for game mode creatures

--- apps/web/app/widgets/vitals-detail/vital-damage-table.component.ts
+++ apps/web/app/widgets/vitals-detail/vital-damage-table.component.ts
@@ -1,31 +1,42 @@
 import { CommonModule } from '@angular/common'
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
-import { getWeaponTagLabel } from '@nw-data/common'
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core'
+import { Affixstats, Damagetable, Statuseffect, Vitals, Vitalsleveldata, Vitalsmodifierdata } from '@nw-data/generated'
 import { combineLatest, map } from 'rxjs'
-import { NwModule } from '~/nw'
+import { NwDbService, NwModule } from '~/nw'
 import { damageTypeIcon } from '~/nw/weapon-types'
 import { IconsModule } from '~/ui/icons'
 import { svgInfo } from '~/ui/icons/svg'
 import { ItemFrameModule } from '~/ui/item-frame'
 import { PropertyGridModule } from '~/ui/property-grid'
 import { TooltipModule } from '~/ui/tooltip'
-import { humanize, rejectKeys, selectStream, shareReplayRefCount } from '~/utils'
+import { humanize, selectStream, shareReplayRefCount } from '~/utils'
 import { ScreenshotModule } from '../screenshot'
 import { VitalDetailStore } from './vital-detail.store'
-import { ComponentStore } from '@ngrx/component-store'
+import { RouterModule } from '@angular/router'
 
 @Component({
   standalone: true,
   selector: 'nwb-vital-damage-table',
   templateUrl: './vital-damage-table.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, NwModule, TooltipModule, PropertyGridModule, IconsModule, ItemFrameModule, ScreenshotModule],
+  imports: [
+    CommonModule,
+    NwModule,
+    TooltipModule,
+    PropertyGridModule,
+    IconsModule,
+    ItemFrameModule,
+    ScreenshotModule,
+    RouterModule,
+  ],
   providers: [VitalDetailStore],
   host: {
     class: 'layout-content',
   },
 })
 export class VitalDamageTableComponent {
+  private db = inject(NwDbService)
+
   @Input()
   public set vitalId(id: string) {
     this.store.patchState({ vitalId: id })
@@ -38,40 +49,35 @@ export class VitalDamageTableComponent {
 
   protected iconInfo = svgInfo
 
-  protected tables$ = selectStream(combineLatest({
-    vital: this.store.vital$,
-    categories: this.store.categories$,
-    tables: this.store.damageTables$,
-    level: this.store.vitalLevel$,
-    modifier: this.store.vitalModifier$
-  }))
+  protected tables$ = selectStream(
+    combineLatest({
+      vital: this.store.vital$,
+      vitalLevel: this.store.vitalLevel$,
+      vitalCategories: this.store.categories$,
+      vitalMod: this.store.vitalModifier$,
+      tables: this.store.damageTables$,
+      affixMap: this.db.affixstatsMap,
+      effectMap: this.db.statusEffectsMap,
+    })
+  )
     .pipe(
-      map(({ vital, level, tables, categories, modifier }) => {
+      map(({ vital, vitalMod, vitalLevel, vitalCategories, tables, affixMap, effectMap }) => {
         return tables.map((table) => {
           return {
             name: table.file.replace(/\.xml$/, '').replace(/^.*javelindata_damagetable/, ''),
             rows: table.rows
               ?.map((row) => {
-                const baseDamage = level.BaseDamage
-                const dmgCoef = row.DmgCoef
-                const dmgMod = vital?.DamageMod || 1
-                const categoryDamageMod = modifier?.CategoryDamageMod || 1
-                const addDmg = row.AddDmg || 0
-
-                // VitalsLevel.BaseDamage * (1 + AIAbilityTable.BaseDamage - AbilityTable.BaseDamageReduction + (Empower)) * DamageTable.DmgCoef * Vitals.DamageMod * VitalsLevel.CategoryDamageMod + DamageTable.AddDmg
-                return {
-                  Name: humanize(row.DamageID.replace(/^Attack/, '')),
-                  DamageTypeName: row.DamageType ? `${row.DamageType}_DamageName` : '',
-                  DamageTypeIcon: damageTypeIcon(row.DamageType),
-                  WeaponCategory: getWeaponTagLabel(row.WeaponCategory) || row.WeaponCategory,
-                  AttackType: row.AttackType,
-                  IsRanged: row.IsRanged,
-                  DmgCoef: row.DmgCoef,
-                  Props: rejectKeys(row, (it) => !row[it]),
-                  Damage: baseDamage * (1 + 0 - 0 + 0) * dmgCoef * dmgMod * categoryDamageMod + addDmg,
-                }
+                return selectDamageInfo({
+                  vital,
+                  vitalLevel,
+                  vitalMod,
+                  damageTable: row,
+                  affixMap,
+                  effectMap,
+                })
               })
-              .sort((a, b) => b.DmgCoef - a.DmgCoef),
+              .filter((it) => !!it.Damage)
+              .sort((a, b) => b.Damage - a.Damage),
           }
         })
       })
@@ -80,5 +86,55 @@ export class VitalDamageTableComponent {
 
   public constructor(private store: VitalDetailStore) {
     //
+  }
+}
+
+function selectDamageInfo({
+  vital,
+  vitalLevel,
+  vitalMod,
+  damageTable,
+  affixMap,
+  effectMap,
+}: {
+  vital: Vitals
+  vitalLevel: Vitalsleveldata
+  vitalMod: Vitalsmodifierdata
+  damageTable: Damagetable
+  affixMap: Map<string, Affixstats>
+  effectMap: Map<string, Statuseffect>
+}) {
+  //
+  const baseDamage = vitalLevel.BaseDamage
+  const dmgCoef = damageTable.DmgCoef
+  const dmgMod = vital?.DamageMod || 1
+  const categoryDamageMod = vitalMod?.CategoryDamageMod || 1
+  const addDmg = damageTable.AddDmg || 0
+
+  // VitalsLevel.BaseDamage * (1 + AIAbilityTable.BaseDamage - AbilityTable.BaseDamageReduction + (Empower)) * DamageTable.DmgCoef * Vitals.DamageMod * VitalsLevel.CategoryDamageMod + DamageTable.AddDmg
+  // TODO: add ability multiplier
+  const totalDamage = baseDamage * (1 + 0 - 0 + 0) * dmgCoef * dmgMod * categoryDamageMod + addDmg
+  const affix = affixMap.get(damageTable.Affixes)
+
+  return {
+    DamageID: damageTable.DamageID,
+    AttackName: humanize(damageTable.DamageID.replace(/^Attack/, '')),
+    AttackType: damageTable.AttackType,
+    IsRanged: damageTable.IsRanged,
+    Damage: totalDamage,
+    Primary: {
+      Type: damageTable.DamageType ? `${damageTable.DamageType}_DamageName` : '',
+      Icon: damageTypeIcon(damageTable.DamageType),
+      Value: totalDamage * (1 - (affix?.DamagePercentage || 0)),
+      Percent: 1 - (affix?.DamagePercentage || 0),
+    },
+    Secondary: affix?.DamagePercentage
+      ? {
+          Type: `${affix.DamageType}_DamageName`,
+          Icon: damageTypeIcon(affix.DamageType),
+          Value: totalDamage * affix.DamagePercentage,
+          Percent: affix.DamagePercentage,
+        }
+      : null,
   }
 }
