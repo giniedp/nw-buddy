290e1886975a58a7a367601eebe35bdbcf2078a6
Ginie <github@ginie.eu>
2023-11-19 22:47:49 +0100

    ui: updated colors

--- apps/web/styles/components/nw-item.js
+++ apps/web/styles/components/nw-item.js
@@ -95,8 +95,8 @@ module.exports = ({ addComponents }) => {
       backgroundRepeat: 'no-repeat',
     },
     '.nw-item-rarity-common': {
-      '--rarity-c0': '#c8c8c8',
-      '--rarity-c1': '#a1a1a1',
+      '--rarity-c0': '#dddddd',
+      '--rarity-c1': '#666666',
       '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquare0.png')`,
       '--rarity-bg-head': `url('${assets}/icons/item/tooltip_header_bg_0.png')`,
       '&.named': {
@@ -104,9 +104,9 @@ module.exports = ({ addComponents }) => {
       },
     },
     '.nw-item-rarity-uncommon': {
-      '--rarity-c0': '#94ff8c',
-      '--rarity-c1': '#1f8a1b',
-      '--rarity-c3': '#1f8a1b',
+      '--rarity-c0': '#34ec5b',
+      '--rarity-c1': '#00ab1a',
+      '--rarity-c3': '#00ab1a',
       '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquare1.png')`,
       '--rarity-bg-head': `url('${assets}/icons/item/tooltip_header_bg_1.png')`,
       '&.named': {
@@ -115,20 +115,20 @@ module.exports = ({ addComponents }) => {
       },
     },
     '.nw-item-rarity-rare': {
-      '--rarity-c0': '#94d9f5',
-      '--rarity-c1': '#1b6f8c',
-      '--rarity-c3': '#1b6f8c',
+      '--rarity-c0': '#72ffff',
+      '--rarity-c1': '#31bdeb',
+      '--rarity-c3': '#31bdeb',
       '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquare2.png')`,
       '--rarity-bg-head': `url('${assets}/icons/item/tooltip_header_bg_2.png')`,
       '&.named': {
-        '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquarenamed2.png"')`,
+        '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquarenamed2.png')`,
         '--rarity-bg-ani': `url('${assets}/icons/item/named_bg_2.webp')`,
       },
     },
     '.nw-item-rarity-epic': {
-      '--rarity-c0': '#d7a3ca',
-      '--rarity-c1': '#8b2490',
-      '--rarity-c3': '#8b2490',
+      '--rarity-c0': '#ff57ff',
+      '--rarity-c1': '#e600de',
+      '--rarity-c3': '#e600de',
       '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquare3.png')`,
       '--rarity-bg-head': `url('${assets}/icons/item/tooltip_header_bg_3.png')`,
       '&.named': {
@@ -137,9 +137,9 @@ module.exports = ({ addComponents }) => {
       },
     },
     '.nw-item-rarity-legendary': {
-      '--rarity-c0': '#ffc67b',
-      '--rarity-c1': '#8f5623',
-      '--rarity-c3': '#8f5623',
+      '--rarity-c0': '#ffa535',
+      '--rarity-c1': '#f07808',
+      '--rarity-c3': '#f07808',
       '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquare4.png')`,
       '--rarity-bg-head': `url('${assets}/icons/item/tooltip_header_bg_4.png')`,
       '&.named': {
@@ -148,9 +148,9 @@ module.exports = ({ addComponents }) => {
       },
     },
     '.nw-item-rarity-artifact': {
-      '--rarity-c0': '#fb7c59',
-      '--rarity-c1': '#991d14',
-      '--rarity-c3': '#991d14',
+      '--rarity-c0': '#ff7c55',
+      '--rarity-c1': '#b42e0a',
+      '--rarity-c3': '#b42e0a',
       '--rarity-bg': `url('${assets}/icons/item/itemraritybgsquareartifact.png')`,
       '--rarity-bg-head': `url('${assets}/icons/item/tooltip_header_bg_artifact.png')`,
       '--rarity-bg-ani': `url('${assets}/icons/item/artifact_bg.webp')`,
