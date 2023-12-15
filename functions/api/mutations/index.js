4ad4a9a849dd9fb5c0189259faa339177cac1e44
Ginie <github@ginie.eu>
2023-12-15 19:56:19 +0100

    new expedition api format

--- functions/api/mutations/index.js
+++ functions/api/mutations/index.js
@@ -1,15 +1,15 @@
 export async function onRequest(context) {
-  const response = await fetch(`https://mutations.5con.club/api/http_trigger`)
-	const data = await response.json()
-  const result = Object.keys(data).map((key) => {
-    const node = data[key]
-    return {
-      expedition: node.Expedition_Id,
-      element: node.Mutation_Id,
-      promotion: node.Promotion,
-      curse: node.Curse_Name,
-    }
-  })
+  const result = await fetch(`https://mutations.5con.club/api/http_trigger`)
+    .then((it) => it.json())
+    .then((it) => it.expeditions)
+    .then((list) => list.map((it) => {
+      return {
+        expedition: it.expedition_id,
+        element: node.mutation_id,
+        promotion: node.promotion,
+        curse: node.curse_name,
+      }
+    }))
   return new Response(JSON.stringify(result), {
     headers: {
       'cache-control': 'max-age=300, private',
