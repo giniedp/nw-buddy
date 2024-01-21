8c7e8cc24cfbe71e934c4d95d92b91ee2ca27160
Ginie <github@ginie.eu>
2024-01-21 08:04:41 +0100

    fix: access nw market prices api

--- functions/api/nwm/servers/[server].js
+++ functions/api/nwm/servers/[server].js
@@ -1,6 +1,11 @@
 export async function onRequest(context) {
   const server = context.params.server
-  const response = await fetch(`https://nwmarketprices.com/api/latest-prices/${server}/`)
+  const url = `https://nwmarketprices.com/api/latest-prices/${server}/`
+  const response = await fetch(url, {
+    headers: {
+      'user-agent': 'NW Buddy Cloudflare Worker'
+    }
+  })
   const result = await response.json()
   return new Response(JSON.stringify(result))
 }
--- functions/api/nwm/servers/index.js
+++ functions/api/nwm/servers/index.js
@@ -1,5 +1,9 @@
 export async function onRequest(context) {
-  const response = await fetch(`https://nwmarketprices.com/api/servers/`)
+  const response = await fetch(`https://nwmarketprices.com/api/servers/`, {
+    headers: {
+      'user-agent': 'NW Buddy Cloudflare Worker'
+    }
+  })
 	const result = await response.json()
   return new Response(JSON.stringify(result))
 }
