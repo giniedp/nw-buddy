3700438ce8bd7019d0256977daabf88ba9fcf616
Ginie <github@ginie.eu>
2024-06-26 00:38:12 +0200

    [electron] reduce app size

--- electron-config.js
+++ electron-config.js
@@ -2,4 +2,9 @@ const config = require('./electron-config.base.json')
 
 module.exports = {
   ...config,
+  files: [
+    ...config.files,
+    "!dist/web-electron/browser/**/worldtiles/**/*",
+    "!dist/web-electron/browser/**/items_hires/**/*"
+  ]
 }
