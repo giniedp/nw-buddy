cbf28a6a1dfd0b269d02f1e5a6547c5407e7f88c
Ginie <github@ginie.eu>
2024-08-30 14:11:53 +0200

    updated icons

--- apps/web/app/ui/icons/svg/sort.ts
+++ apps/web/app/ui/icons/svg/sort.ts
@@ -1,1 +1,1 @@
-export const svgSort = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 -960 960 960\" width=\"24\">\r\n  <path fill=\"currentColor\" d=\"M120-240v-80h240v80H120Zm0-200v-80h480v80H120Zm0-200v-80h720v80H120Z\" />\r\n</svg>\r\n"
+export const svgSort = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 -960 960 960\" width=\"24\"> <path fill=\"currentColor\" d=\"M120-240v-80h240v80H120Zm0-200v-80h480v80H120Zm0-200v-80h720v80H120Z\" /> </svg> "
