9804588c941cb0f36c150ac856b8099c0f040c6a
Ginie <github@ginie.eu>
2025-01-07 20:07:01 +0100

    implemented pocketbase adapter

---
+++ apps/web/app/data/backend/pocketbase/auth-state.ts
@@ -0,0 +1,15 @@
+import PocketBase, { AuthRecord, RecordModel, RecordService } from 'pocketbase'
+import { Observable } from 'rxjs'
+
+export interface AuthState {
+  token: string
+  record: AuthRecord
+}
+
+export function authState(client: PocketBase): Observable<AuthState> {
+  return new Observable<AuthState>((sub) => {
+    return client.authStore.onChange((token, record) => {
+      sub.next({ token, record})
+    }, true)
+  })
+}
--- apps/web/app/data/backend/provider.ts
+++ apps/web/app/data/backend/provider.ts
@@ -1,10 +1,15 @@
 import { inject, InjectionToken, Type } from '@angular/core'
+import { environment } from '../../../environments'
 import type { BackendAdapter } from './backend-adapter'
 import { NoBackendAdapter } from './no-backend'
+import { PocketbaseAdapter } from './pocketbase/pocketbase-adapter'
 
 export const BACKEND_ADAPTER = new InjectionToken<BackendAdapter>('BACKEND_ADAPTER', {
   providedIn: 'root',
   factory: () => {
+    if (environment.pocketbaseUrl) {
+      return inject(PocketbaseAdapter)
+    }
     return inject(NoBackendAdapter)
   },
 })
