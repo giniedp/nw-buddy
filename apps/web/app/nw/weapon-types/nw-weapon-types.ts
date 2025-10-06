279afbb17b0b09e0aa340ad07a7ef87ef6b9f100
Ginie <github@ginie.eu>
2025-10-06 21:45:34 +0200

    refactoring

--- apps/web/app/nw/weapon-types/nw-weapon-types.ts
+++ apps/web/app/nw/weapon-types/nw-weapon-types.ts
@@ -1,9 +1,17 @@
-import { DamageType, StatusEffectCategory } from '@nw-data/generated'
+import { DamageType, ProgressionCategory, StatusEffectCategory, WeaponTag } from '@nw-data/generated'
 import { CaseInsensitiveMap, eqCaseInsensitive } from '~/utils'
 
 export interface NwWeaponType {
+  // TODO: get rid of `WeaponTypeID` and use `WeaponTag` everywhere instead
+  // however `WeaponTypeID` is still used in skill tree records
+
   WeaponTypeID: string
-  WeaponTag: string
+  WeaponTag: WeaponTag
+  ProgressionId: ProgressionCategory
+  StatsRef: string
+  DamageType: DamageType
+  DamageTablePrefix: string
+
   UIName: string
   MasteryName: string
   Tree1Name: string
@@ -12,259 +20,288 @@ export interface NwWeaponType {
   CategoryName: string
   IconPath: string
   IconPathSmall: string
-  DamageType: DamageType
-  DamageTablePrefix: string
-  StatsRef: string
-  ProgressionId: string
 }
 
 export const NW_WEAPON_TYPES: Array<NwWeaponType> = [
   // ONE HANDED
   {
+    WeaponTypeID: 'Swords',
+    WeaponTag: 'Sword',
+    ProgressionId: 'SwordAbilityTable',
+    StatsRef: 'SwordT5',
+    DamageType: 'Slash',
+    DamageTablePrefix: '1H_Sword_',
+
     UIName: 'ui_straightsword',
     MasteryName: 'ui_straightsword_mastery',
     Tree1Name: 'ui_sword',
     Tree2Name: 'ui_shield',
-    WeaponTypeID: 'Swords',
-    WeaponTag: 'Sword',
-    DamageType: 'Slash',
     GroupName: 'Swords_GroupName',
     CategoryName: 'ui_onehanded_weapons',
     IconPath: 'assets/icons/weapons/1hsword.png',
     IconPathSmall: 'assets/icons/weapons/1hswordsmall.png',
-    DamageTablePrefix: '1H_Sword_',
-    StatsRef: 'SwordT5',
-    ProgressionId: 'SwordAbilityTable',
   },
   {
+    WeaponTypeID: 'Rapiers',
+    WeaponTag: 'Rapier',
+    ProgressionId: 'RapierAbilityTable',
+    StatsRef: 'RapierT5',
+    DamageType: 'Thrust',
+    DamageTablePrefix: '1H_Rapier_',
+
     UIName: 'ui_rapier',
     MasteryName: 'ui_rapier_mastery',
     Tree1Name: 'ui_blood',
     Tree2Name: 'ui_evade',
-    WeaponTypeID: 'Rapiers',
-    WeaponTag: 'Rapier',
-    DamageType: 'Thrust',
     GroupName: 'Rapiers_GroupName',
     CategoryName: 'ui_onehanded_weapons',
     IconPath: 'assets/icons/weapons/1hrapier.png',
     IconPathSmall: 'assets/icons/weapons/1hrapiersmall.png',
-    DamageTablePrefix: '1H_Rapier_',
-    StatsRef: 'RapierT5',
-    ProgressionId: 'RapierAbilityTable',
   },
   {
+    WeaponTypeID: 'Hatchets',
+    WeaponTag: 'Axe',
+    ProgressionId: 'HatchetAbilityTable',
+    StatsRef: 'HatchetT5',
+    DamageType: 'Slash',
+    DamageTablePrefix: 'Hatchet_',
+
     UIName: 'ui_hatchet',
     MasteryName: 'ui_hatchet_mastery',
     Tree1Name: 'ui_hatchet_tree1',
     Tree2Name: 'ui_hatchet_tree2',
-    WeaponTypeID: 'Hatchets',
-    WeaponTag: 'Axe',
-    DamageType: 'Slash',
     GroupName: 'Hatchets_GroupName',
     CategoryName: 'ui_onehanded_weapons',
     IconPath: 'assets/icons/weapons/1hhatchet.png',
     IconPathSmall: 'assets/icons/weapons/1hhatchetsmall.png',
-    DamageTablePrefix: 'Hatchet_',
-    StatsRef: 'HatchetT5',
-    ProgressionId: 'HatchetAbilityTable',
   },
   {
+    WeaponTypeID: 'Flail',
+    WeaponTag: 'Flail',
+    ProgressionId: 'FlailAbilityTable',
+    StatsRef: 'FlailT5',
+    DamageType: 'Strike',
+    DamageTablePrefix: '1H_Flail_',
+
     UIName: 'ui_flail',
     MasteryName: 'ui_flail_mastery',
     Tree1Name: 'ui_flail_tree1',
     Tree2Name: 'ui_flail_tree2',
-    WeaponTypeID: 'Flail',
-    WeaponTag: 'Flail',
-    DamageType: 'Strike',
     GroupName: 'Flails_GroupName',
     CategoryName: 'ui_onehanded_weapons',
     IconPath: 'assets/icons/weapons/1hflail.png',
     IconPathSmall: 'assets/icons/weapons/1hflailsmall.png',
-    DamageTablePrefix: '1H_Flail_',
-    StatsRef: 'FlailT5',
-    ProgressionId: 'FlailAbilityTable',
   },
   // TWO HANDED
   {
+    WeaponTypeID: 'Spears',
+    WeaponTag: 'Spear',
+    ProgressionId: 'SpearAbilityTable',
+    StatsRef: 'SpearT5',
+    DamageType: 'Thrust',
+    DamageTablePrefix: '2H_Spear_',
+
     UIName: 'ui_spear',
     MasteryName: 'ui_spear_mastery',
     Tree1Name: 'ui_spear_tree1',
     Tree2Name: 'ui_spear_tree2',
-    WeaponTypeID: 'Spears',
-    WeaponTag: 'Spear',
-    DamageType: 'Thrust',
     GroupName: 'Spears_GroupName',
     CategoryName: 'ui_twohanded_weapons',
     IconPath: 'assets/icons/weapons/speara.png',
     IconPathSmall: 'assets/icons/weapons/spearasmall.png',
-    DamageTablePrefix: '2H_Spear_',
-    StatsRef: 'SpearT5',
-    ProgressionId: 'SpearAbilityTable',
   },
   {
+    WeaponTypeID: 'GreatAxe',
+    WeaponTag: 'GreatAxe',
+    ProgressionId: 'GreatAxeAbilityTable',
+    StatsRef: '2HAxeT5',
+    DamageType: 'Slash',
+    DamageTablePrefix: 'GreatAxe_',
+
     UIName: 'ui_greataxe',
     MasteryName: 'ui_greataxe_mastery',
     Tree1Name: 'ui_greataxe_tree1',
     Tree2Name: 'ui_greataxe_tree2',
-    WeaponTypeID: 'GreatAxe',
-    WeaponTag: 'GreatAxe',
-    DamageType: 'Slash',
     GroupName: 'GreatAxe_GroupName',
     CategoryName: 'ui_twohanded_weapons',
     IconPath: 'assets/icons/weapons/2hgreataxe.png',
     IconPathSmall: 'assets/icons/weapons/2hgreataxesmall.png',
-    DamageTablePrefix: 'GreatAxe_',
-    StatsRef: '2HAxeT5',
-    ProgressionId: 'GreatAxeAbilityTable',
   },
   {
+    WeaponTypeID: 'WarHammers',
+    WeaponTag: 'Warhammer',
+    ProgressionId: 'WarHammerAbilityTable',
+    StatsRef: '2HHammerT5',
+    DamageType: 'Strike',
+    DamageTablePrefix: 'Warhammer_',
+
     UIName: 'ui_warhammer',
     MasteryName: 'ui_warhammer_mastery',
     Tree1Name: 'ui_juggernaut',
     Tree2Name: 'ui_crowdcrusher',
-    WeaponTypeID: 'WarHammers',
-    WeaponTag: 'Warhammer',
-    DamageType: 'Strike',
     GroupName: 'WarHammers_GroupName',
     CategoryName: 'ui_twohanded_weapons',
     IconPath: 'assets/icons/weapons/2hdemohammer.png',
     IconPathSmall: 'assets/icons/weapons/2hdemohammersmall.png',
-    DamageTablePrefix: 'Warhammer_',
-    StatsRef: '2HHammerT5',
-    ProgressionId: 'WarHammerAbilityTable',
   },
   {
+    WeaponTypeID: 'Greatsword',
+    WeaponTag: 'Greatsword',
+    ProgressionId: 'GreatSwordAbilityTable',
+    StatsRef: '2hGreatSwordT5',
+    DamageType: 'Slash',
+    DamageTablePrefix: 'GreatSword_',
+
     UIName: 'ui_greatsword',
     MasteryName: 'ui_greatsword_mastery',
     Tree1Name: 'ui_greatsword_tree1',
     Tree2Name: 'ui_greatsword_tree2',
-    WeaponTypeID: 'Greatsword',
-    WeaponTag: 'Greatsword',
-    DamageType: 'Slash',
     GroupName: 'Greatsword_GroupName',
     CategoryName: 'ui_twohanded_weapons',
     IconPath: 'assets/icons/weapons/2hgreatsword.png',
     IconPathSmall: 'assets/icons/weapons/2hgreatswordsmall.png',
-    DamageTablePrefix: 'GreatSword_',
-    StatsRef: '2hGreatSwordT5',
-    ProgressionId: 'GreatSwordAbilityTable',
   },
 
   // RANGED
   {
+    WeaponTypeID: 'Muskets',
+    WeaponTag: 'Rifle',
+    ProgressionId: 'MusketAbilityTable',
+    StatsRef: 'MusketT5',
+    DamageType: 'Thrust',
+    DamageTablePrefix: 'Musket',
+
     UIName: 'ui_musket',
     MasteryName: 'ui_musket_mastery',
     Tree1Name: 'ui_musket_tree1',
     Tree2Name: 'ui_musket_tree2',
-    WeaponTypeID: 'Muskets',
-    WeaponTag: 'Rifle',
-    DamageType: 'Thrust',
     GroupName: 'Muskets_GroupName',
     CategoryName: 'ui_ranged_weapons',
     IconPath: 'assets/icons/weapons/2hmusketa.png',
     IconPathSmall: 'assets/icons/weapons/2hmusketasmall.png',
-    DamageTablePrefix: 'Musket',
-    StatsRef: 'MusketT5',
-    ProgressionId: 'MusketAbilityTable',
   },
   {
+    WeaponTypeID: 'Bows',
+    WeaponTag: 'Bow',
+    ProgressionId: 'BowAbilityTable',
+    StatsRef: 'BowT5',
+    DamageType: 'Thrust',
+    DamageTablePrefix: 'Bow',
+
     UIName: 'ui_bow',
     MasteryName: 'ui_bow_mastery',
     Tree1Name: 'ui_bow_tree1',
     Tree2Name: 'ui_bow_tree2',
-    WeaponTypeID: 'Bows',
-    WeaponTag: 'Bow',
-    DamageType: 'Thrust',
     GroupName: 'Bows_GroupName',
     CategoryName: 'ui_ranged_weapons',
     IconPath: 'assets/icons/weapons/bowb.png',
     IconPathSmall: 'assets/icons/weapons/bowbsmall.png',
-    DamageTablePrefix: 'Bow',
-    StatsRef: 'BowT5',
-    ProgressionId: 'BowAbilityTable',
   },
   {
+    WeaponTypeID: 'Blunderbuss',
+    WeaponTag: 'Blunderbuss',
+    ProgressionId: 'BlunderbussAbilityTable',
+    StatsRef: 'BlunderbussT5',
+    DamageType: 'Thrust',
+    DamageTablePrefix: 'Blunderbuss_',
+
     UIName: 'ui_blunderbuss',
     MasteryName: 'ui_blunderbuss_mastery',
     Tree1Name: 'ui_blunderbuss_tree1',
     Tree2Name: 'ui_blunderbuss_tree2',
-    WeaponTypeID: 'Blunderbuss',
-    WeaponTag: 'Blunderbuss',
-    DamageType: 'Thrust',
     GroupName: 'Blunderbuss_GroupName',
     CategoryName: 'ui_ranged_weapons',
     IconPath: 'assets/icons/weapons/blunderbuss.png',
     IconPathSmall: 'assets/icons/weapons/blunderbusssmall.png',
-    DamageTablePrefix: 'Blunderbuss_',
-    StatsRef: 'BlunderbussT5',
-    ProgressionId: 'BlunderbussAbilityTable',
   },
 
   // MAGIC
   {
+    WeaponTypeID: 'StavesFire',
+    WeaponTag: 'Fire',
+    ProgressionId: 'FireMagicAbilityTable',
+    StatsRef: 'FireStaffT5',
+    DamageType: 'Fire',
+    DamageTablePrefix: 'Firestaff_',
+
     UIName: 'ui_firestaff',
     MasteryName: 'ui_firemagic_mastery',
     Tree1Name: 'ui_firestaff_tree1',
     Tree2Name: 'ui_firestaff_tree2',
-    WeaponTypeID: 'StavesFire',
-    WeaponTag: 'Fire',
-    DamageType: 'Fire',
     GroupName: 'StavesFire_GroupName',
     CategoryName: 'ui_magic_skills',
     IconPath: 'assets/icons/weapons/stafffire.png',
     IconPathSmall: 'assets/icons/weapons/stafffiresmall.png',
-    DamageTablePrefix: 'Firestaff_',
-    StatsRef: 'FireStaffT5',
-    ProgressionId: 'FireMagicAbilityTable',
   },
   {
+    WeaponTypeID: 'StavesLife',
+    WeaponTag: 'Heal',
+    ProgressionId: 'LifeMagicAbilityTable',
+    StatsRef: 'LifeStaffT5',
+    DamageType: 'Nature',
+    DamageTablePrefix: 'LifeStaff_',
+
     UIName: 'ui_lifestaff',
     MasteryName: 'ui_lifemagic_mastery',
     Tree1Name: 'ui_lifestaff_tree1',
     Tree2Name: 'ui_lifestaff_tree2',
-    WeaponTypeID: 'StavesLife',
-    WeaponTag: 'Heal',
-    DamageType: 'Nature',
     GroupName: 'StavesLife_GroupName',
     CategoryName: 'ui_magic_skills',
     IconPath: 'assets/icons/weapons/stafflife.png',
     IconPathSmall: 'assets/icons/weapons/stafflifesmall.png',
-    DamageTablePrefix: 'LifeStaff_',
-    StatsRef: 'LifeStaffT5',
-    ProgressionId: 'LifeMagicAbilityTable',
   },
   {
+    WeaponTypeID: 'GauntletIce',
+    WeaponTag: 'Ice',
+    ProgressionId: 'IceMagicAbilityTable',
+    StatsRef: '1hElementalGauntlet_IceT5',
+    DamageType: 'Ice',
+    DamageTablePrefix: 'IceMagic_',
+
     UIName: 'ui_icemagic',
     MasteryName: 'ui_icemagic_mastery',
     Tree1Name: 'ui_icemagic_tree1',
     Tree2Name: 'ui_icemagic_tree2',
-    WeaponTypeID: 'GauntletIce',
-    WeaponTag: 'Ice',
-    DamageType: 'Ice',
     GroupName: 'GauntletIce_GroupName',
     CategoryName: 'ui_magic_skills',
     IconPath: 'assets/icons/weapons/icegauntlet.png',
     IconPathSmall: 'assets/icons/weapons/icegauntletsmall.png',
-    DamageTablePrefix: 'IceMagic_',
-    StatsRef: '1hElementalGauntlet_IceT5',
-    ProgressionId: 'IceMagicAbilityTable',
   },
   {
+    WeaponTypeID: 'GauntletVoid',
+    WeaponTag: 'VoidGauntlet',
+    ProgressionId: 'VoidGauntletAbilityTable',
+    StatsRef: 'VoidGauntletT5',
+    DamageType: 'Corruption',
+    DamageTablePrefix: 'VoidGauntlet_',
+
     UIName: 'ui_voidmagic',
     MasteryName: 'ui_voidmagic_mastery',
     Tree1Name: 'ui_voidmagic_tree1',
     Tree2Name: 'ui_voidmagic_tree2',
-    WeaponTypeID: 'GauntletVoid',
-    WeaponTag: 'VoidGauntlet',
-    DamageType: 'Corruption',
     GroupName: 'GauntletVoid_GroupName',
     CategoryName: 'ui_magic_skills',
     IconPath: 'assets/icons/weapons/voidgauntlet.png',
     IconPathSmall: 'assets/icons/weapons/voidgauntletsmall.png',
-    DamageTablePrefix: 'VoidGauntlet_',
-    StatsRef: 'VoidGauntletT5',
-    ProgressionId: 'VoidGauntletAbilityTable',
   },
+  // Add new weapon types here
+  // {
+  //   WeaponTypeID:      -> preferably use same as `WeapoTag`
+  //   WeaponTag:         -> should be given from generated types
+  //   ProgressionId:     -> should be given from generated types. It likely is 'WEAPONTAGT5',
+  //   StatsRef:          -> lookup what new weapons are linked to. It likely is 'WEAPONTAGT5',
+  //   DamageType:        -> should be given from generated types
+  //   DamageTablePrefix: -> lookup damagetables and work out the prefix. It likely is 'WAPONTAG_',
+
+  //   UIName: 'ui_voidmagic',
+  //   MasteryName: 'ui_voidmagic_mastery',
+  //   Tree1Name: 'ui_voidmagic_tree1',
+  //   Tree2Name: 'ui_voidmagic_tree2',
+  //   GroupName: 'GauntletVoid_GroupName',
+  //   CategoryName: 'ui_magic_skills',
+  //   IconPath: 'assets/icons/weapons/voidgauntlet.png',
+  //   IconPathSmall: 'assets/icons/weapons/voidgauntletsmall.png',
+  // },
 ]
 
 export const NW_DAMAGE_TYPE_ICONS = new CaseInsensitiveMap(
@@ -341,8 +378,8 @@ export function statusEffectCategoryIcon(type: string) {
   return NW_SE_CATEGORY_ICONS.get(type)
 }
 
-export function getWeaponTypeInfo(byWeaponTag: string) {
-  return NW_WEAPON_TYPES.find((it) => eqCaseInsensitive(it.WeaponTag, byWeaponTag))
+export function getWeaponTypeInfo(weaponTag: string) {
+  return NW_WEAPON_TYPES.find((it) => eqCaseInsensitive(it.WeaponTag, weaponTag))
 }
 
 export function getWeaponTypes() {
