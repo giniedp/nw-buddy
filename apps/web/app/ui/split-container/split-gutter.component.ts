211a4e0f451d074a7ecf11a5be0c94e1fb6996f4
Alexander Gräfenstein <github@ginie.eu>
2026-07-06 22:33:26 +0200

    minor styling

--- apps/web/app/ui/split-container/split-gutter.component.ts
+++ apps/web/app/ui/split-container/split-gutter.component.ts
@@ -7,14 +7,14 @@ import { svgGripDotsVertical } from '../icons/svg'
   selector: 'nwb-split-gutter',
   template: `
     <button class="btn btn-sm btn-ghost btn-square cursor-grab" (mousedown)="start.next($event)">
-      <nwb-icon [icon]="icon" class="w-5 h-5"/>
+      <nwb-icon [icon]="icon" class="w-5 h-5" />
     </button>
   `,
   host: {
-    class: 'divider divider-horizontal w-0 m-0 z-10',
+    class: 'divider divider-horizontal w-0! m-0 z-10',
   },
   exportAs: 'gutter',
-  imports: [IconsModule]
+  imports: [IconsModule],
 })
 export class SplitGutterComponent {
   public elRef = inject<ElementRef<HTMLElement>>(ElementRef)
